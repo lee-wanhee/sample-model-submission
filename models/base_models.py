ad70a9de40a2c9ff970108e05d0ef9d292dfca1a
Wanhee Lee <wanhee@stanford.edu>
2023-06-09 14:26:19 -0700

    video input investigation

--- models/base_models.py
+++ models/base_models.py
@@ -10,18 +10,32 @@ from model_tools.activations.pytorch import PytorchWrapper
 from transformers import AutoImageProcessor, ViTMAEModel
 from transformers import ViTImageProcessor, ViTModel
 from transformers import AutoFeatureExtractor, ResNetModel
+from transformers import AutoImageProcessor, VideoMAEModel
+from huggingface_hub import hf_hub_download
+import av
 from PIL import Image
 import requests
 
 import numpy as np
+import torch
 
 """
 Template module for a base model submission to brain-score
 """
 
-def load_preprocess_images(image_filepaths, image_size, **kwargs):
+def load_preprocess_images(image_filepaths, image_size, processor=None, **kwargs):
     images = load_images(image_filepaths)
-    images = preprocess_images(images, image_size=image_size, **kwargs)
+    # images = [<PIL.Image.Image image mode=RGB size=400x400 at 0x7F8654B2AC10>, ...]
+    if processor is not None:
+        images = [processor(images=image, return_tensors="pt") for image in images]
+        if len(images[0].keys()) != 1:
+            raise NotImplementedError(f'unknown processor for getting model {processor}')
+        assert list(images[0].keys())[0] == 'pixel_values'
+        images = [image['pixel_values'] for image in images]
+        images = torch.cat(images)
+        images = images.cpu().numpy()
+    else:
+        images = preprocess_images(images, image_size=image_size, **kwargs)
     return images
 
 def load_images(image_filepaths):
@@ -60,6 +74,20 @@ def torchvision_preprocess(normalize_mean=(0.485, 0.456, 0.406), normalize_std=(
         lambda img: img.unsqueeze(0)
     ])
 
+def create_static_video(image, num_frames):
+    '''
+    Create a static video with the same image in all frames.
+    Args:
+        image (PIL.Image.Image): Input image.
+        num_frames (int): Number of frames in the video.
+    Returns:
+        result (np.ndarray): np array of frames of shape (num_frames, height, width, 3).
+    '''
+    frames = []
+    for _ in range(num_frames):
+        frame = np.array(image)
+        frames.append(frame)
+    return np.stack(frames)
 
 
 def get_model_list():
@@ -69,9 +97,10 @@ def get_model_list():
     If the submission contains only one model, return a one item list.
     :return: a list of model string names
     """
-    
-    # return ['mae_vitb16_1',  'mae_vitl16_1'] # 'videomae-vitb16-videoinput' 0: layer[1, 12) / 1: layer[0, 12)
-    return ['dinov1_vits16_1', 'dinov1_vits8_1', 'dinov1_vitb16_1', 'dinov1_vitb8_1'] #,  #, 'dino_resnet-50_0']
+    # 0: layer[1, 12) / 1: layer[0, 12) / 2: original model's preprocessor
+    return ['videomae_vitb16_videoinput_1']
+    # return ['mae_vitb16_1',  'mae_vitl16_1'] # 'videomae-vitb16-videoinput'
+    # return ['dinov1_vits16_1', 'dinov1_vits8_1', 'dinov1_vitb16_1', 'dinov1_vitb8_1'] #,  #, 'dino_resnet-50_0']
     # ['mae-vitb16', 'videomae', 'dino', 'clip', 'vc-1', 'vip', 'vit', 'timesformer', 'deit', 'sam', 'dpt', 'cvt']
 
 
@@ -89,8 +118,10 @@ def get_model(name):
         # https://github.com/huggingface/transformers/blob/main/src/transformers/models/vit_mae/modeling_vit_mae.py
         if 'vitb' in name:
             model = ViTMAEModel.from_pretrained("facebook/vit-mae-base")
+            processor = AutoImageProcessor.from_pretrained("facebook/vit-mae-base")
         elif 'vitl' in name:
             model = ViTMAEModel.from_pretrained("facebook/vit-mae-large")
+            processor = AutoImageProcessor.from_pretrained("facebook/vit-mae-large")
         else:
             raise NotImplementedError(f'unknown model for getting model {name}')
 
@@ -102,10 +133,6 @@ def get_model(name):
         # last_hidden_states = outputs.last_hidden_state
 
         # breakpoint()
-
-        preprocessing = functools.partial(load_preprocess_images, image_size=224)
-        wrapper = PytorchWrapper(identifier=name, model=model, preprocessing=preprocessing)
-        wrapper.image_size = 224
     
     elif model_name == 'dinov1':
         # https://huggingface.co/facebook/dino-vitb16
@@ -141,20 +168,57 @@ def get_model(name):
 
         # breakpoint()
 
-        preprocessing = functools.partial(load_preprocess_images, image_size=224)
-        wrapper = PytorchWrapper(identifier=name, model=model, preprocessing=preprocessing)
-        wrapper.image_size = 224
-
     elif model_name == 'videomae':
-        raise NotImplementedError(f'unknown model for getting model {name}')
-        if name.split('_')[-1] == 'videoinput':
-            preprocessing = functools.partial(load_preprocess_images, image_size=224)
-            preprocessing = lambda x: preprocessing(x).unsqueeze(0).repeat(12, 1, 1, 1)
+        if name.split('_')[2] == 'videoinput':
+            def read_video_pyav(container, indices):
+                '''
+                Decode the video with PyAV decoder.
+                Args:
+                    container (`av.container.input.InputContainer`): PyAV container.
+                    indices (`List[int]`): List of frame indices to decode.
+                Returns:
+                    result (np.ndarray): np array of decoded frames of shape (num_frames, height, width, 3).
+                '''
+                frames = []
+                container.seek(0)
+                start_index = indices[0]
+                end_index = indices[-1]
+                for i, frame in enumerate(container.decode(video=0)):
+                    if i > end_index:
+                        break
+                    if i >= start_index and i in indices:
+                        frames.append(frame)
+                return np.stack([x.to_ndarray(format="rgb24") for x in frames])
+            breakpoint()
+            
+            file_path = hf_hub_download(
+                repo_id="nielsr/video-demo", filename="eating_spaghetti.mp4", repo_type="dataset"
+            )
+            container = av.open(file_path)
+            # indices = sample_frame_indices(clip_len=16, frame_sample_rate=1, seg_len=container.streams.video[0].frames)
+            indices = [i for i in range(16)]
+            video = read_video_pyav(container, indices)
+            # video = np.array with shape (16, 360, 640, 3), max value 255, dtype uint8
+            image_processor = AutoImageProcessor.from_pretrained("MCG-NJU/videomae-base")
+            inputs = image_processor(list(video), return_tensors="pt")
+            breakpoint()
+
+            def processor(images=None, return_tensors="pt"):
+                image_processor = AutoImageProcessor.from_pretrained("MCG-NJU/videomae-base")
+                # change PIL.Image to torch.Tensor
+                num_frames = 16
+                video = create_static_video(image, num_frames)
+                inputs = image_processor(list(video), return_tensors=return_tensors)
+                return images
         else:
             raise NotImplementedError(f'unknown model for getting model {name}')
 
     else:
         raise NotImplementedError(f'unknown model for getting model {name}')
+
+    preprocessing = functools.partial(load_preprocess_images, processor=processor, image_size=224)
+    wrapper = PytorchWrapper(identifier=name, model=model, preprocessing=preprocessing)
+    wrapper.image_size = 224
 
     return wrapper
 
@@ -177,17 +241,17 @@ def get_layers(name):
     if 'vits' in model_backbone:
         for i in range(12): # 12
             layers.append(f'encoder.layer.{i}.output')
-            layers.append(f'encoder.layer.{i}.layernorm_before')
+            # layers.append(f'encoder.layer.{i}.layernorm_before')
         layers.append('layernorm')
     elif 'vitb' in model_backbone:
         for i in range(12): # 12
             layers.append(f'encoder.layer.{i}.output')
-            layers.append(f'encoder.layer.{i}.layernorm_before')
+            # layers.append(f'encoder.layer.{i}.layernorm_before')
         layers.append('layernorm')
     elif 'vitl' in model_backbone:
         for i in range(24): # 24
             layers.append(f'encoder.layer.{i}.output')
-            layers.append(f'encoder.layer.{i}.layernorm_before')
+            # layers.append(f'encoder.layer.{i}.layernorm_before')
         layers.append('layernorm')
     else:
         raise NotImplementedError(f'unknown model for getting layers {name}')
